bf5dd992130aa8c172277d232c1939851ae751ff
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-21 01:26:24 -0600

    program complete.

--- 29_War_Preparations/Program.cs
+++ 29_War_Preparations/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace _29_War_Preparations
+﻿namespace _29_War_Preparations;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Sword sword = new Sword(Material.Iron, Gemstone.None, 10, 2);
+        Sword s2 = sword with { mat = Material.Steel, gem = Gemstone.Emerald, length = 300, xGuardWidth = 0 };
+        Sword s3 = sword with { mat = Material.Binarium, gem = Gemstone.Bitstone };
+
+        Console.ReadLine();
     }
 }
+
+public enum Material
+{
+    Wood, Bronze, Iron, Steel, Binarium
+}
+
+public enum Gemstone
+{
+    Emerald, Amber, Sapphire, Diamond, Bitstone, None
+}
+
+public record Sword (Material mat, Gemstone gem, int length, int xGuardWidth);
