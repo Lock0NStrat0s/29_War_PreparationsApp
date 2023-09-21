180a535b0440d3d0aa453e3961bc8e89d9abc28f
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-21 01:05:44 -0600

    Add project files.

---
+++ 29_War_Preparations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _29_War_Preparations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
